f34a306910dd208e294bc0f05b67bc1df62ce188
thallissousa <thallissousa@outlook.com>
2022-04-12 13:52:17 -0300

    Initial Commit

---
+++ VideoTVCast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoTVCast
+//
+//  Created by Thallis Sousa on 12/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoTVCast/VideoTVCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoTVCastApp.swift
+//  VideoTVCast
+//
+//  Created by Thallis Sousa on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoTVCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
